966d9c525ad793aeb049b879b7c4adc07ab45572
Allyson Wesman <allysonwesman@gmail.com>
2018-03-05 18:34:49 -0800

    Write signup tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ApplicationController do
 
   describe "Homepage" do
-    it 'loads the homepage', :focus => true do
+    it 'loads the homepage' do
       get '/'
       expect(last_response.status).to eq(200)
       expect(last_response.body).to include("Welcome to Snippet Manager")
