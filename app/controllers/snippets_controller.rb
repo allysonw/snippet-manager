07742ec09a96c5c1fba717159e3cac813d7cc69c
Allyson Wesman <allysonwesman@gmail.com>
2018-03-07 11:25:53 -0800

    get edit and delete working

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -56,11 +56,13 @@ class SnippetsController < ApplicationController
     end
   end
 
-  post "/snippets/:id" do
+  patch "/snippets/:id" do
     @snippet = Snippet.find_by(id: params[:id])
 
     if !params[:name].empty? && !params[:content].empty?
-      @snippet.update(params)
+      @snippet.update(name: params[:name], content: params[:content], language: params[:language], access_level: params[:access_level])
+      @user = current_user
+
       erb :"snippets/show"
     else
       redirect to "/snippets/#{@snippet.id}/edit"
@@ -69,7 +71,7 @@ class SnippetsController < ApplicationController
 
   # ----- DELETE -----
 
-  post "/snippets/:id/delete" do
+  delete "/snippets/:id/delete" do
     @snippet = Snippet.find_by(id: params[:id])
 
     if logged_in? && current_user.snippets.include?(@snippet)
@@ -79,7 +81,4 @@ class SnippetsController < ApplicationController
       redirect to "/snippets"
     end
   end
-
-
-
 end
